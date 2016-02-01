15ea1ad6e844811137d2bd1475cf6ad003ca9b6f
root <root@debian.lalya>
2016-02-01 23:23:12 +0100

    semaine 2

--- run_shoot.py
+++ run_shoot.py
@@ -5,20 +5,19 @@ from soccersimulator import BaseStrategy, SoccerAction
 from soccersimulator import SoccerTeam, SoccerMatch
 from soccersimulator import Vector2D, Player, SoccerTournament
 import strategy
+from strategy import Mystate
 class fonceurStrategy(BaseStrategy):
     def __init__(self):
         BaseStrategy.__init__(self, "fonceur")
     def compute_strategy(self, state, id_team, id_player):
         #si id_team1 à la balle il shoote, sinn il cours juste vers la balle, de même pour id_team2
+        mystate = Mystate(state,id_team,id_player)
         if id_team == 1:
-            #action = strategy.shoot_ball_team1(state, id_team, id_player)
-            action = strategy.position_goalkeeper(state, id_team, id_player)
+            return strategy.gardien(mystate)
         elif id_team == 2:
-            action = strategy.shoot_ball_team2(state, id_team, id_player)
+            return strategy.fonceur(mystate)
+        return SoccerAction()
             
-        action_bis = action.copy()
-        action_ter = action + action_bis
-        return SoccerAction(action_ter.acceleration, action_ter.shoot)
 
 team1 = SoccerTeam("team1", [Player("t1j1", fonceurStrategy())])
 team2 = SoccerTeam("team2", [Player("t2j1", fonceurStrategy())])
--- strategy.py
+++ strategy.py
@@ -1,57 +1,80 @@
 # -*-coding: utf8 -*
 import soccersimulator
 from soccersimulator.settings import *
-from soccersimulator import BaseStrategy, SoccerAction
-from soccersimulator import SoccerTeam, SoccerMatch
-from soccersimulator import Vector2D, Player, SoccerTournament
+from soccersimulator import SoccerAction, Vector2D, Player
 
 #DEFINITION DES CONSTANTES
 GOAL_SURFACE_WIDTH = GAME_WIDTH/13.
 
-def position_ball(state):
-    """Position de la balle"""
-    return state.ball.position
+class Mystate:
+    def __init__(self, state, id_team, id_player):
+        self.state = state
+        self.id_team = id_team
+        self.id_player =id_player
 
-def position_player(state, id_team, id_player):
-    """Position du joueur"""
-    return state.player_state(id_team, id_player).position
+    @property
+    def position_ball(self):
+        """Position de la balle"""
+        return self.state.ball.position
 
-def fonce_ball(state, id_team, id_player):
-    """Fonce vers la balle"""
-    return SoccerAction(position_ball(state) - position_player(state, id_team, id_player))
+    @property
+    def position_player(self):
+        """Position du joueur"""
+        return self.state.player_state(self.id_team,self.id_player).position
 
-def distance_player_ball(state, id_team, id_player):
-    """Distance entre le joueur et la balle	"""
-    return position_ball(state).distance(position_player(state, id_team, id_player))
+    @property
+    def goal_un(self):
+        """Position du goal1, c'est à dire à gauche"""
+        return Vector2D(x = 0, y = GAME_HEIGHT/2.)
 
-def rayon_player_ball():
-    """Rayon du joueur plus rayon de la balle"""
-    return PLAYER_RADIUS + BALL_RADIUS 
-	
-def shoot_ball_team1(state, id_team, id_player):
-    """Shoot la balle vers la droite"""
-    if distance_player_ball(state, id_team, id_player) < rayon_player_ball():
-	return SoccerAction(position_ball(state) - position_player(state, id_team, id_player), Vector2D(x = GAME_WIDTH, y = GAME_HEIGHT/2.) - position_ball(state))
-    else:
-        return fonce_ball(state, id_team, id_player)
+    @property
+    def goal_deux(self):
+        """Position du goal2, c'est à dire à droite"""
+        return Vector2D(x = GAME_WIDTH, y = GAME_HEIGHT/2.)
 
-def shoot_ball_team2(state, id_team, id_player):
-    """Shoot la balle vers la gauche"""
-    if distance_player_ball(state, id_team, id_player) < rayon_player_ball():
-	return SoccerAction(position_ball(state) - position_player(state, id_team, id_player), Vector2D(x = 0, y = GAME_HEIGHT/2.) - position_ball(state))
-    else:
-        return fonce_ball(state, id_team, id_player)
+    @property
+    def distance_player_ball(self):
+        """Distance entre le joueur et la balle	"""
+        return self.position_ball.distance(self.position_player)
 
-def position_goalkeeper(state, id_team, id_player):
-    """Position du gardien"""
-    if distance_player_ball(state, id_team, id_player) < GOAL_SURFACE_WIDTH:
-        return shoot_ball_team1(state, id_team, id_player)
-    elif position_ball(state).y <= GAME_HEIGHT - 50 and position_ball(state).x >= GAME_WIDTH/11.:
-        return SoccerAction(Vector2D(x = 1, y = (GAME_HEIGHT/2.) - 2) -
-        position_player(state, id_team, id_player)) 
-    elif position_ball(state).y >= GAME_HEIGHT - 40 and position_ball(state).x >= GAME_WIDTH/11.:
-        return SoccerAction(Vector2D(x = 1, y = (GAME_HEIGHT/2.) + 2) -
-        position_player(state, id_team, id_player))
-    else:
-        return SoccerAction(Vector2D(x = 1, y = GAME_HEIGHT/2.) - position_player(state, id_team, id_player))
+    @property
+    def rayon_player_ball(self):
+        """Rayon du joueur plus rayon de la balle"""
+        return PLAYER_RADIUS + BALL_RADIUS
 
+    @property
+    def fonce_ball(self):
+        """Fonce vers la balle"""
+        return SoccerAction(self.position_ball - self.position_player)
+
+    @property
+    def shoot_ball(self):
+        """Shoot la balle vers la droite"""
+        if self.id_team == 1:
+            if self.distance_player_ball < self.rayon_player_ball:
+                return SoccerAction((self.position_ball - self.position_player), (self.goal_deux - self.position_ball))
+            else:
+                return self.fonce_ball
+        elif self.id_team == 2:
+            if self.distance_player_ball < self.rayon_player_ball:
+                return SoccerAction((self.position_ball - self.position_player), (self.goal_un - self.position_ball))
+            else:
+                return self.fonce_ball
+
+    @property
+    def position_goalkeeper(self):
+        """Position du gardien"""
+        if self.distance_player_ball < GOAL_SURFACE_WIDTH:
+            return self.shoot_ball
+        elif (self.position_ball.y <= GAME_HEIGHT - 50) and (self.position_ball.x >= GAME_WIDTH/11.):
+            return SoccerAction((self.goal_un - 2) - self.position_player) 
+        elif (self.position_ball.y >= GAME_HEIGHT) - 40 and (self.position_ball.x >= GAME_WIDTH/11.):
+            return SoccerAction((self.goal_un + 2) - self.position_player)
+        
+
+
+def fonceur(mystate):
+    return mystate.fonce_ball + mystate.shoot_ball
+
+def gardien(mystate):
+    return mystate.position_goalkeeper
