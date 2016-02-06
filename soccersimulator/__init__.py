d74f35f632b7d30f39fd742e7642e8ce837be075
Ahmed <baldeahmedtidiane36@gmail.com>
2016-02-07 00:34:52 +0100

    Delete __init__.py

--- soccersimulator/__init__.py
+++
@@ -1,5 +0,0 @@
-from utils import Vector2D, MobileMixin
-from mdpsoccer import SoccerAction, Configuration, SoccerTeam, SoccerState, Player
-from mdpsoccer import SoccerMatch, SoccerEvents, BaseStrategy, SoccerTournament, Score, KeyboardStrategy
-from interfaces import MatchWindow, show
-import settings
