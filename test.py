8ff73648407af7efa571894c7840d44b05ab51b2
BALDE AHMED TIDIANE <3502264@ppti-14-308-10.ufr-info-p6.jussieu.fr>
2016-02-15 19:45:40 +0100

    Keyboard

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from soccersimulator import show
 from soccersimulator import SoccerMatch
 from soccersimulator import KeyboardStrategy
 from team import lalya1, lalya2, lalya4, lalya1bis
+from coordination import *
 
 match_lalya1 = SoccerMatch(lalya1, lalya1)
 match_lalya2 = SoccerMatch(lalya2, lalya2)
@@ -10,4 +11,8 @@ match_lalya4 = SoccerMatch(lalya4, lalya4)
 
 match_lalya5 = SoccerMatch(lalya4, lalya2)
 if __name__ == "__main__":
+    strat = KeyboardStrategy(fn="monfichier.exp")
+    #fn veut dire filename
+    strat.add("d", defenseur)
+    #Ensuite on ajoute les joueurs, bref...
     show(match_lalya5)
