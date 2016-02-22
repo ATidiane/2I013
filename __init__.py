1491997603ecb45aeefb9c18b4d5dd5b46935107
BALDE AHMED TIDIANE <3502264@ppti-14-308-10.ufr-info-p6.jussieu.fr>
2016-02-22 19:58:47 +0100

    Reglage d'erreu dans __init__

--- __init__.py
+++ __init__.py
@@ -3,4 +3,4 @@ import soccersimulator
 from soccersimulator.settings import *
 from soccersimulator import BaseStrategy, SoccerAction
 from soccersimulator import SoccerTournament
-from team import lalya1, lalya2, lalya4, lalya0
+from team import lalya1 as team1, lalya2 as team2, lalya4 as team4, lalya0 as team0
